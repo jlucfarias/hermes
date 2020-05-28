734eec86ed690047cae9ae20cd89db09b49d3358
jlucfarias <jlalmeida.farias@gmail.com>
2020-05-28 16:59:25 -0300

    Init project

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// import Page from './pages/Page';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* <Route path="/" exact component={Page} /> */}
+      </ Switch>
+    </ BrowserRouter>
+  );
+}
