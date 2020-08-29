9ae98fa0a5a2d833cc4f4b3e6cdfd7382ec760a7
jlucfarias <jlalmeida.farias@gmail.com>
2020-08-29 18:14:40 -0300

    fix: solve path errors

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -9,14 +9,14 @@ export default function Menu() {
   return (
     <header className="Menu-container">
       <nav>
-        <Link to="/feed">Feed</Link>
+        <Link to="/">Feed</Link>
         <Link to="/library">My Library</Link>
         <Link to="/settings">Settings</Link>
       </nav>
       <Link to="/">
         <img src={logo} alt="Hermes Logo"/>
       </Link>
-      <a href="/about">About</a>
+      <a href="/about.html">About</a>
     </header>
   );
 }
